d7c33d64a41a1736c052d1ddb5f769e327f53a90
Nur Izzati Binti Abd Haris <nurizzatibintiabdharis@Nurs-MacBook-Pro.local>
2019-03-27 12:53:59 +0100

    firebase-react

---
+++ firebase/firebase.js
@@ -0,0 +1,38 @@
+import * as firebase from "firebase";
+
+//first projet = development database
+const devConfig = {
+    apiKey: "AIzaSyC4l1ODnl1dhTlUN5zT8ZbcjTuKX43A8-0",
+    authDomain: "ldapptest-34664.firebaseapp.com",
+    databaseURL: "https://ldapptest-34664.firebaseio.com",
+    projectId: "ldapptest-34664",
+    storageBucket: "ldapptest-34664.appspot.com",
+    messagingSenderId: "983554343988"
+}
+
+//second projet = production database
+const prodConfig = {
+    apiKey: "AIzaSyBYzBRkKSZT3w7dh5bv9KSy7JuwA714oXk",
+    authDomain: "ldapp-4878a.firebaseapp.com",
+    databaseURL: "https://ldapp-4878a.firebaseio.com",
+    projectId: "ldapp-4878a",
+    storageBucket: "",
+    messagingSenderId: "844278515788"
+}
+
+//definie config soit dev soit prod (suivant c'est if else)
+const config = process.env.NODE_ENV === "production"
+    ? prodConfig
+    : devConfig;
+
+if(!firebase.apps.length){
+    firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export {
+    db,
+    auth,
+};
