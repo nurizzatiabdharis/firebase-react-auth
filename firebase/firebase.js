83832409b383775d69080ba0a6eb0ec6bafe43ba
Nur Izzati BINTI ABD HARIS <48798235+nurizzatiabdharis@users.noreply.github.com>
2020-10-26 12:19:49 +0100

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,22 +2,22 @@ import * as firebase from "firebase";
 
 //1ere projet = development database
 const devConfig = {
-    apiKey: "AIzaSyC4l1ODnl1dhTlUN5zT8ZbcjTuKX43A8-",
-    authDomain: "ldapptest-34664.firebaseapp.com",
-    databaseURL: "https://ldapptest-34664.firebaseio.com",
-    projectId: "ldapptest-34664",
-    storageBucket: "ldapptest-34664.appspot.com",
-    messagingSenderId: "983554343988"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 }
 
 //2eme projet = production database
 const pr0odConfig = {
-    apiKey: "AIzaSyBYzBRkKSZT3w7dh5bv9KSy7JuwA714oXk",
-    authDomain: "ldapp-4878a.firebaseapp.com",
-    databaseURL: "https://ldapp-4878a.firebaseio.com",
-    projectId: "ldapp-4878a",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "844278515788"
+    messagingSenderId: ""
 }
 
 //definie config soit dev soit prod (suivant c'est if else)
